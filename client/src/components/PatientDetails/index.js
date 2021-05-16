fe63e3873b00514e84da4c6f8ad1b0319fa2a41a
Sushant Pandey <skp000002@gmail.com>
2021-05-16 19:26:28 +0530

    Add patient address

--- client/src/components/PatientDetails/index.js
+++ client/src/components/PatientDetails/index.js
@@ -6,6 +6,7 @@ import styles from "./index.module.css";
 function PatientDetails() {
   const [patient_name, setPatient_name] = useState("");
   const [patient_phone, setPatient_phone] = useState("");
+  const [patient_address, setPatient_address] = useState("")
 
   const history = useHistory();
   useEffect(() => {
@@ -18,6 +19,7 @@ function PatientDetails() {
     e.preventDefault();
     window.localStorage.setItem("patient_name", patient_name);
     window.localStorage.setItem("patient_phone", patient_phone);
+    window.localStorage.setItem("patient_address", patient_address);
     history.push("/patient");
   }
 
@@ -34,7 +36,10 @@ function PatientDetails() {
             <p>Mobile No</p>
             <input className={styles.input} value={patient_phone} onChange={(e) => setPatient_phone(e.target.value)} type="text" />
           </div>
-
+          <div className={styles.formRow}>
+            <p>Address</p>
+            <input className={styles.input} value={patient_address} onChange={(e) => setPatient_address(e.target.value)} type="text" />
+          </div>
           <div>
             <button type="submit" onClick={(e) => handleSubmit(e)}>Get Help!</button>
           </div>
