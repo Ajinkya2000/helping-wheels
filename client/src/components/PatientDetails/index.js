a7525abdfb51bb744442817d85954619899301f0
Sushant Pandey <skp000002@gmail.com>
2021-05-16 16:48:43 +0530

    Axios

--- client/src/components/PatientDetails/index.js
+++ client/src/components/PatientDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router";
 
 import styles from "./index.module.css";
@@ -9,10 +9,17 @@ function PatientDetails() {
 
   const history = useHistory();
   useEffect(() => {
-    if (window.localStorage.getItem("phone")) {
+    if (window.localStorage.getItem("patient_phone")) {
       history.push("/patient");
     }
   }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.localStorage.setItem("patient_name", patient_name);
+    window.localStorage.setItem("patient_phone", patient_phone);
+    history.push("/patient");
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -21,15 +28,15 @@ function PatientDetails() {
         <form action="">
           <div className={styles.formRow}>
             <p>Name</p>
-            <input className={styles.input} value={patient_name} onChange={(newName) => setPatient_name(newName)} type="text" />
+            <input className={styles.input} value={patient_name} onChange={(e) => setPatient_name(e.target.value)} type="text" />
           </div>
           <div className={styles.formRow}>
             <p>Mobile No</p>
-            <input className={styles.input} value={patient_phone} onChange={(newPhone) => setPatient_phone(newPhone)} type="text" />
+            <input className={styles.input} value={patient_phone} onChange={(e) => setPatient_phone(e.target.value)} type="text" />
           </div>
 
           <div>
-            <button type="submit">Get Help!</button>
+            <button type="submit" onClick={(e) => handleSubmit(e)}>Get Help!</button>
           </div>
         </form>
       </div>
