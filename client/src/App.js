5aa66dd9fcb7800be25b150b6dec9ad30c707277
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-05-16 13:17:59 +0530

    add service worker

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import Register from "./components/Register/Register";
 // import PatientScreen from './components/PatientScreen';
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
 function App() {
+  useEffect(() => {
+    const beamsClient = new PusherPushNotifications.Client({
+      instanceId: "8d0baa0c-08b4-43ce-9134-e9fe4b14ec6a",
+    });
+
+    const func = async () => {
+      beamsClient.start().then((beamsClient) => beamsClient.getDeviceId())
+      const userId = await beamsClient.getUserId();
+      console.log(userId)
+      // .getUserId().then((userId) => console.log(userId));
+
+    }
+    func();
+
+
+    // beamsClient
+    //   .start()
+    //   .then((beamsClient) => beamsClient.getDeviceId())
+    //   .then((deviceId) =>
+    //     console.log("Successfully registered with Beams. Device ID:", deviceId)
+    //   )
+    //   .catch(console.error);
+  }, []);
+
   return (
     <div className="App">
       {/*<PatientScreen />*/}
       <Register />
-
     </div>
   );
 }
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,34 +1,95 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import axios from "axios";
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
 const Register = () => {
-    const [name, setName] = useState("")
-    const [phone, setPhone] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [vehicle, setVehicle_name] = useState("")
-    const [vehicle_number, setVehicle_num] = useState(null)
-    const [address, setAddress] = useState("")
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [vehicle, setVehicle_name] = useState("");
+  const [vehicle_number, setVehicle_num] = useState(null);
+  const [address, setAddress] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const res = await axios.post("http://127.0.0.1:8000/api/register/", {name, phone, email, password, vehicle, vehicle_number, address,
-                                                                                    "latitude": 34.384, "longitude": 66.541})
-        console.log(res)
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://127.0.0.1:8000/api/register/", {
+      name,
+      phone,
+      email,
+      password,
+      vehicle,
+      vehicle_number,
+      address,
+      latitude: 34.384,
+      longitude: 66.541,
+    });
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name"  value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="number" placeholder="Mobile Number" value={phone} onChange={(e) => setPhone(e.target.value)} />
-            <input type="email" placeholder="Email"  value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <input type="text" placeholder="Vehicle Name" value={vehicle} onChange={(e) => setVehicle_name(e.target.value)} />
-            <input type="text" placeholder="Vehicle Number" value={vehicle_number} onChange={(e) => setVehicle_num(e.target.value)} />
-            <input type="address" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
-            <button type="submit" >Submit</button>
-        </form>
-    )
+    localStorage.setItem("token", res.data.token);
 
-}
-export default Register
+    const tokenProvider = new PusherPushNotifications.TokenProvider({
+      url: "http://127.0.0.1:8000/api/pusher/",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    const beamsClient = new PusherPushNotifications.Client({
+      instanceId: "8d0baa0c-08b4-43ce-9134-e9fe4b14ec6a",
+    });
+
+    beamsClient
+      .start()
+      .then((beamsClient) => beamsClient.setUserId(`${res.data.user.id}`, tokenProvider))
+      .catch(console.error);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Mobile Number"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Vehicle Name"
+        value={vehicle}
+        onChange={(e) => setVehicle_name(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Vehicle Number"
+        value={vehicle_number}
+        onChange={(e) => setVehicle_num(e.target.value)}
+      />
+      <input
+        type="address"
+        placeholder="Address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default Register;
