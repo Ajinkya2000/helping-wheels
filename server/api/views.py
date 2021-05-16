ec156e43e3757acf7d48c51f39ea9e6ab6ca7fe2
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-05-16 17:54:18 +0530

    add patient views

--- server/api/views.py
+++ server/api/views.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .utils import create_otp, send_otp, filter_volunteer_by_location
 from .models import User
 from math import radians
-from .utils import get_user_from_token
+from .utils import get_user_from_token, get_pusher_token
+from pusher_push_notifications import PushNotifications
 
 OTP = None
 
@@ -25,7 +26,7 @@ class RegisterUserView(APIView):
     def post(request):
         global OTP
         otp = request.data.pop('otp')
-        if otp != OTP:
+        if int(otp) != OTP:
             return Response({'error': 'Enter correct OTP', }, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = UserSerializer(data={**request.data, "is_verified": True})
@@ -41,7 +42,8 @@ class UpdateUser(APIView):
     @staticmethod
     def patch(request):
         user = get_user_from_token(request)
-        serializer = UserSerializer(instance=user, data=request.data, partial=True)
+        serializer = UserSerializer(
+            instance=user, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
@@ -57,3 +59,44 @@ class Locator(APIView):
         userList = filter_volunteer_by_location(queryset, lat1, lon1)
         serializer = UserSerializer(userList, many=True)
         return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
+
+class GetPusherToken(APIView):
+    def get(self, request):
+        user = get_user_from_token(request)
+        pusher_token = get_pusher_token(user_id=str(user.id))
+        return Response(pusher_token, status=status.HTTP_200_OK)
+
+
+class GetNotification(APIView):
+    def post(self, request):
+        beams_client = PushNotifications(
+            instance_id='8d0baa0c-08b4-43ce-9134-e9fe4b14ec6a',
+            secret_key='6EC51704541E3F5D0BD2D6C3C0A40DF677AB58EA12B28B701A935DC98DCFEFB3',
+        )
+
+        qs = request.data['volunteers']
+        userIds = [str(user.id) for user in qs]
+        userIds.append('19')
+
+        response = beams_client.publish_to_users(
+            user_ids=userIds,
+            publish_body={
+                'web': {
+                    'notification': {
+                        'title': 'yo',
+                        'body': 'nice',
+                    }
+                }
+            }
+        )
+
+        return Response({response}, status=status.status.HTTP_200_OK)
+
+
+class PatientView(APIView):
+    def get(self, request):
+        user = get_user_from_token(request)
+        patient = Patient.objects.get(user=user.id)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data, status=status.HTTP_200_OK)
