8f8461eb118764f492a20373e2f63c8b4fdeac98
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-05-16 15:49:34 +0530

    add dynamic markers

--- server/env.bat
+++
@@ -1,4 +0,0 @@
-set DJANGO_SECRET_KEY="django-insecure-dd50=(s3hp!3t=my7$+1%cj=!@mbz+7qgs10m72&p67zp6+00u"
-set EMAIL_HOST_USER='agrawal.kashish1907@gmail.com'
-set EMAIL_HOST_PASSWORD='cezrzzrtmtwokcmy'
-set JWT_KEY='amancenator'
