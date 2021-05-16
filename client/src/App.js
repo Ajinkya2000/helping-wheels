edff7868d07ea163a314c22cc283fad2a2045321
ryuzaki221 <52002984+ryuzaki221@users.noreply.github.com>
2021-05-16 13:21:58 +0530

    add patient detail design

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PatientDetails from "./components/PatientDetails";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <PatientDetails />
+    </div>
+  );
 }
 
 export default App;
